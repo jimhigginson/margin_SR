0b5b1086c5be719fb23e33138270d2cea5cd731b
Jim Higginson <jah119@ic.ac.uk>
2022-06-16 16:14:36 +0100

    updated lots

--- meta-analysis/qualitative.py
+++ meta-analysis/qualitative.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import numpy as np
 
-data_file = './all_data_unverified_for_testing.csv'
+data_file = './2022-06-13_final_consensus_data.csv'
 # Change this to final data
 
 data = pd.read_csv(data_file)
 #Imports data
 
-data = data.loc[data['Reviewer Name'] == 'James Higginson']
 # includes only data from those reviewed by me. Change in final analysis to 'Consensus'
 
 supporting_text = data.filter(regex='supporting', axis=1)
@@ -16,8 +15,7 @@ supporting_text.index=data['Study ID']
 cols_to_keep = [
        'Study ID', 
        'Title', 
-       'First Author Name', 
-       'Year of publication',
+       'modality_group',
        'Could the selection of patients have introduced bias?',
        'Is there concern that the included patients do not match the review question?',
        'Could the conduct or interpretation of the index test have introduced bias?',
@@ -29,3 +27,4 @@ cols_to_keep = [
 ]
 
 data = data[cols_to_keep]
+data = data.drop_duplicates(subset='Title', ignore_index=True)
